6d753e5949e3ca1f841f1cfef37000ac671165e0
Oliver Schulz <oschulz@mpp.mpg.de>
2021-10-24 13:47:24 +0200

    Add disable-conda.sh

--- provisioning/install-sw-scripts/anaconda3-setup.sh
+++ provisioning/install-sw-scripts/anaconda3-setup.sh
@@ -8,6 +8,23 @@ pkg_installed_check() {
 }
 
 
+install_disable_conda() {
+# Function "remove_from_path" is a variation of a solution by Mark Booth,
+# see https://unix.stackexchange.com/a/291611:
+
+cat > "${1}/bin/disable-conda.sh" <<-EOF
+function remove_from_path {
+    # Delete path by parts so we can never accidentally remove sub paths
+    export PATH=\${PATH//":\$1:"/":"} # delete any instances in the middle
+    export PATH=\${PATH/#"\$1:"/} # delete any instance at the beginning
+    export PATH=\${PATH/%":\$1"/} # delete any instance in the at the end
+}
+
+(command -v conda > /dev/null) && remove_from_path "${1}/bin" && remove_from_path "${1}/condabin"
+EOF
+}
+
+
 pkg_install() {
     DOWNLOAD_URL="https://repo.continuum.io/archive/Anaconda3-${PACKAGE_VERSION}-Linux-x86_64.sh"
     echo "INFO: Download URL: \"${DOWNLOAD_URL}\"." >&2
@@ -23,6 +40,8 @@ pkg_install() {
     # Install mamba via micromamba since conda solver takes ages or fails to do it:
     wget -qO- https://anaconda.org/conda-forge/micromamba/0.17.0/download/linux-64/micromamba-0.17.0-0.tar.bz2 | tar -xvj bin/micromamba
     bin/micromamba -r "${INSTALL_PREFIX}" install -y mamba -n base -c conda-forge
+
+    install_disable_conda "${INSTALL_PREFIX}"
 }
 
 

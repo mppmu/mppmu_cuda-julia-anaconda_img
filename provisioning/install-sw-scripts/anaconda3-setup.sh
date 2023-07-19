ad1831616057dd955c23224138fb33ba0eb2a611
Oliver Schulz <oschulz@mpp.mpg.de>
2023-07-19 12:58:32 +0200

    Update Anaconda install script

--- provisioning/install-sw-scripts/anaconda3-setup.sh
+++ provisioning/install-sw-scripts/anaconda3-setup.sh
@@ -38,7 +38,7 @@ pkg_install() {
     mv "${INSTALL_PREFIX}/bin"/*-config "${INSTALL_PREFIX}/devbin"
 
     # Install mamba via micromamba since conda solver takes ages or fails to do it:
-    wget -qO- https://anaconda.org/conda-forge/micromamba/1.1.0/download/linux-64/micromamba-1.1.0-0.tar.bz2 | tar -xvj bin/micromamba
+    wget -qO- https://anaconda.org/conda-forge/micromamba/1.4.9/download/linux-64/micromamba-1.4.9-0.tar.bz2 | tar -xvj bin/micromamba
     bin/micromamba -r "${INSTALL_PREFIX}" install -y mamba -n base -c conda-forge
 
     install_disable_conda "${INSTALL_PREFIX}"
